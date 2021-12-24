35934962f14fb2871a8036fb82d6ca145f292730
charles.zhao <charles.zhao@klook.com>
2021-12-24 15:41:37 +0800

    删除没用的变量名

--- dao/user_dao.go
+++ dao/user_dao.go
@@ -16,16 +16,16 @@ func GetUser(id *string, name *string, password *string, status *int) error {
 	return err
 }
 
-func CreateUser(user model.User) (result sql.Result, err error) {
+func CreateUser(user model.User) (sql.Result, error) {
 	if user.ID != "" {
 		return db.DB.Exec(`INSERT INTO users (id, name, password, status) VALUES (?, ?, ?, ?)`, user.ID, user.Name, user.Password, user.Status)
 	} else {
 		return db.DB.Exec(`INSERT INTO users (name, password, status) VALUES (?, ?, ?)`, user.Name, user.Password, user.Status)
 	}
 }
-func UpdateUser(user model.User) (result sql.Result, err error) {
+func UpdateUser(user model.User) (sql.Result, error) {
 	return db.DB.Exec(`UPDATE users set name = ?, password =?, status=? where id=?`, user.Name, user.Password, user.Status, user.ID)
 }
-func DeleteUser(id string) (result sql.Result, err error) {
+func DeleteUser(id string) (sql.Result, error) {
 	return db.DB.Exec(`DELETE FROM users WHERE id = ?`, id)
 }
