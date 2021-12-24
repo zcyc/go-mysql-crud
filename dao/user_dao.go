622d99a379ab2428a87218fca121a373be5dec8c
charles.zhao <charles.zhao@klook.com>
2021-12-24 15:41:59 +0800

    换行

--- dao/user_dao.go
+++ dao/user_dao.go
@@ -23,9 +23,11 @@ func CreateUser(user model.User) (sql.Result, error) {
 		return db.DB.Exec(`INSERT INTO users (name, password, status) VALUES (?, ?, ?)`, user.Name, user.Password, user.Status)
 	}
 }
+
 func UpdateUser(user model.User) (sql.Result, error) {
 	return db.DB.Exec(`UPDATE users set name = ?, password =?, status=? where id=?`, user.Name, user.Password, user.Status, user.ID)
 }
+
 func DeleteUser(id string) (sql.Result, error) {
 	return db.DB.Exec(`DELETE FROM users WHERE id = ?`, id)
 }
